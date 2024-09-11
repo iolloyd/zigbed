4df35f269b12e19ff68c752279522d9d0c0e1202
Lloyd Moore <manchesterboy@gmail.com>
2024-09-11 11:50:20 +0100

    project builder

---
+++ build.zig
@@ -0,0 +1,26 @@
+const std = @import("std");
+
+pub fn build(b: *std.build.Builder) void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+
+    const exe = b.addExecutable(.{
+        .name = "embedding-generator",
+        .root_source_file = .{ .path = "src/main.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+
+    exe.linkLibC();
+    exe.addIncludePath(.{ .path = "/path/to/onnxruntime/include" });
+    exe.addLibraryPath(.{ .path = "/path/to/onnxruntime/lib" });
+    exe.linkSystemLibrary("onnxruntime");
+
+    b.installArtifact(exe);
+
+    const run_cmd = b.addRunArtifact(exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+
+    const run_step = b.step("run", "Run the app");
+    run_step.dependOn(&run_cmd.step);
+}
